818763ba8df3c1303f3a320027ce33f6b407dcd8
lipkerton <petr.petuxof@yandex.ru>
2026-05-09 14:55:05 +0300

    session added

--- app/database/database.py
+++ app/database/database.py
@@ -1,6 +1,12 @@
-from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 
 from ..config import settings
 
 
 engine = create_async_engine(settings.postgres_url)
+sessionmaker = async_sessionmaker(bind=engine, expire_on_commit=False)
+
+
+def get_db():
+    with sessionmaker() as session:
+        yield session
