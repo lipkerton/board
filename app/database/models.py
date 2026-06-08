650913347599fa7498eac98fc5a7b4798caecdce
lipkerton <petr.petuxof@yandex.ru>
2026-06-08 14:54:33 +0300

    new model

--- app/database/models.py
+++ app/database/models.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from sqlalchemy import Column, BigInteger, String, Text, Integer, DateTime, ForeignKey
+from sqlalchemy import Column, BigInteger, String, Text, Integer, DateTime, ForeignKey, func
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy.ext.asyncio import AsyncAttrs
 
@@ -14,7 +14,7 @@ class Post(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     title: Mapped[str] = mapped_column(String(255))
     content: Mapped[str] = mapped_column(String(5000))
-    created_at: Mapped[datetime] = mapped_column(DateTime, server_default=datetime.now())
+    created_at: Mapped[datetime] = mapped_column(DateTime, server_default=func.CURRENT_TIMESTAMP())
     user_id: Mapped[str] = mapped_column(ForeignKey("users.id"))
 
 
@@ -22,4 +22,4 @@ class User(Base):
     __tablename__ = "users"
     id: Mapped[int] = mapped_column(primary_key=True)
     daily_id: Mapped[int] = mapped_column(Integer)
-    created_at: Mapped[datetime] = mapped_column(DateTime, server_default=datetime.now())
+    created_at: Mapped[datetime] = mapped_column(DateTime, server_default=func.CURRENT_TIMESTAMP())
